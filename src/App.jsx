0bcef960f26dc1ba587e7818a1836846d20add4a
Golagani Rashmika <golagani.rashmika@accenture.com>
2025-02-10 15:22:27 +0530

    created a new file for the login page and written the basic  html code static version of react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Log in</h1>
+      <h2>to start learning</h2>
+    </div>
+  );
+}
